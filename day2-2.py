65807296e7265fe235bfb6cb7b57d0cd721e4660
Daniel Nieto <danielnieto89@gmail.com>
2021-12-02 19:15:18 -0600

    complete day 2

---
+++ day2-2.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Command:
+    direction: str
+    value: int
+
+
+class Submarine:
+    def __init__(self):
+        self.depth = 0
+        self.position = 0
+        self.aim = 0
+        self.ops = {"forward": self.forward, "up": self.up, "down": self.down}
+
+    def do(self, command: Command):
+        self.ops[command.direction](command.value)
+
+    def forward(self, value):
+        self.position += value
+        self.depth += self.aim * value
+
+    def up(self, value):
+        self.aim -= value
+
+    def down(self, value):
+        self.aim += value
+
+
+commands = []
+
+with open("day2-1-input.txt", "r") as file:
+    for line in file:
+        direction, value = line.strip().split(" ")
+        commands.append(Command(direction, int(value)))
+
+submarine = Submarine()
+
+for command in commands:
+    submarine.do(command)
+
+print(submarine.depth * submarine.position)
